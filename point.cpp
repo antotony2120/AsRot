3dbe8527a23b396df01001c9c74dc3606f465c66
antony <antotony@yandex.ru>
2018-10-05 15:49:26 +0300

    Add new class point

--- point.cpp
+++ point.cpp
@@ -2,5 +2,58 @@
 
 Point::Point()
 {
+    this->_x = 0;
+    this->_y = 0;
+    this->_z = 0;
+}
 
+Point::Point(double _x, double _y, double _z){
+    this->_x = _x;
+    this->_y = _y;
+    this->_z = _z;
+}
+
+Point::Point(Point& copypoint){
+    this->_x = copypoint.x();
+    this->_y = copypoint.y();
+    this->_z = copypoint.z();
+}
+
+Point::~Point(){
+    this->_x = 0;
+    this->_y = 0;
+    this->_z = 0;
+}
+
+double Point::x(){
+    return this->_x;
+}
+
+double Point::y(){
+    return this->_y;
+}
+
+double Point::z(){
+    return this->_z;
+}
+
+Point& Point::operator + (Point& move){
+    this->_x += move.x();
+    this->_y += move.y();
+    this->_z += move.z();
+    return *this;
+}
+
+Point& Point::operator - (Point& move){
+    this->_x -= move.x();
+    this->_y -= move.y();
+    this->_z -= move.z();
+    return *this;
+}
+
+Point& Point::operator = (Point& copypoint){
+    this->_x = copypoint.x();
+    this->_y = copypoint.y();
+    this->_z = copypoint.z();
+    return *this;
 }
--- point.h
+++ point.h
@@ -4,8 +4,19 @@
 
 class Point
 {
+private:
+    double _x, _y, _z;
 public:
     Point();
+    Point(double _x, double _y, double _z);
+    Point(Point& copypoint);
+    ~Point();
+    double x();
+    double y();
+    double z();
+    Point& operator + (Point& move);
+    Point& operator - (Point& move);
+    Point& operator = (Point& copypoint);
 };
 
 #endif // POINT_H
